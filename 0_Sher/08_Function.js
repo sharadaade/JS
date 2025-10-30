dbe0102c7762aa1fab83d73b6f78242f86a2609e
sharadaade <aadesharad@email.com>
2025-10-30 23:14:08 +0530

    Some notes added

--- 0_Sher/08_Function.js
+++ 0_Sher/08_Function.js
@@ -1,1 +1,8 @@
-prompt()
+// Write a JS code to input a sentence and get individual words reverse
+// input - JS is so Easy
+// output - SJ si os ysaE
+
+let words = 'JS is so Easy';
+let splString = words.split(" ");
+
+
