e08b17c98867e3005d3bba4d3f23c2b84554c6de
sharadaade <aadesharad@email.com>
2025-11-12 19:37:08 +0530

    day 6 task

---
+++ 01_JSLab/07_Day7.js
@@ -0,0 +1,78 @@
+// let add = (a ,b) => {
+//     return a + b;
+// }
+// console.log(add(2,4));
+
+// let ar = [1, 2, 3, 4, 5];
+// console.log(typeof ar);
+
+
+// let result = (...a) => {
+//     let r = 0;
+//     for (let i = 0; i < a.length; i++) {
+//         r += a[i];
+//     }
+//     return  r;
+// }
+// console.log(result(1,2,3,4,));
+
+// function add() {
+//     let a = document.getElementById('first').value
+//     let b = document.getElementById('second').value
+
+//     let r = Number(a) + Number(b);
+
+//     let result = document.getElementById('ok')
+//     result.innerHTML = r;
+// }
+
+
+
+// let addArr = (arr) => {
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         sum = sum + arr[i];
+//     }
+//     return sum;
+// }
+//0 1 2 3
+// let x = [8,9];
+// let result = addArr(x)
+// console.log(result);
+
+// let User = {
+//     name: 'Core',
+//     age: 22,
+//     city: 'Heaven'
+// }
+// let fun = (obj) => {
+//     console.log(`name: ${obj.name} age: ${obj.age} city: ${obj.city}`);
+// }
+// fun(User)
+
+// let obj = [
+//     {
+//         name: 'demon',
+//         rollNum: 101,
+//         marks: 99
+//     },
+//     {
+//         name: 'Raj',
+//         rollNum: 102,
+//         marks: 70
+//     },
+//     {
+//         name: 'Vid',
+//         rollNum: 103,
+//         marks: 88
+//     }
+// ]
+
+// let print = (arObj) => {
+//     for (let i = 0; i < arObj.length; i++) {
+//         console.log(`name: ${arObj[i].name} rollNum: ${arObj[i].rollNum} Marks: ${arObj[i].marks} `);
+//     }
+// }
+// print(obj);
+
+
