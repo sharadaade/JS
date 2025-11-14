d61991e5fae1add0bcff54b3bff7f6f6a5355952
sharadaade <aadesharad@email.com>
2025-11-15 02:09:14 +0530

    change background color project

---
+++ JSProject/changeBackground.js
@@ -0,0 +1,20 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        // console.log(e.target.id);
+        if(e.target.id == 'gray'){
+            body.style.backgroundColor = e.target.id
+        }
+        if(e.target.id == 'white'){
+            body.style.backgroundColor = e.target.id
+        }
+        if(e.target.id == 'blue'){
+            body.style.backgroundColor = e.target.id
+        }
+        if(e.target.id == 'yellow'){
+            body.style.backgroundColor = e.target.id
+        }
+    });
+});
